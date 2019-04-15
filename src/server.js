91a41843aad9598ce9a31fac4b1ef8a3e1d02237
recofka <recofka@gmail.com>
2019-04-15 09:52:34 +0200

    First commit at a basic server

---
+++ src/server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const app = express();
+
+app.use.apply(express.json());
+
+app.get("/", (req, res) =>{
+    return res.send("basic server is running")
+});
+
+app.listen(5000);
