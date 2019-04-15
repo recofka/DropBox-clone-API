aee6e14d3a64f89f12d7461f5e5d9141f920927b
recofka <recofka@gmail.com>
2019-04-15 11:08:31 +0200

    Created Box model

---
+++ src/models/Box.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+const Box = new mongoose.Schema({
+    title:{
+        type: String,
+        required: true
+    },
+    files:[]
+}, {
+    timestamps: true
+});
+
+module.exports = mongoose.model("Box", Box)
