348042ad7d42c3ed2f610bdf4e33db188add4d78
recofka <recofka@gmail.com>
2019-04-15 14:53:34 +0200

    I added and configured socke.io so that all users have automatic updates

--- src/server.js
+++ src/server.js
@@ -1,9 +1,21 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const path = require('path');
+const cors = require('cors');
 
 const app = express();
+app.use(cors());
 
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+
+io.on('connection', socket => {
+    socket.on('connectRoom', box => {
+        socket.join(box);
+    });
+});
+
+//Conection with mongo
 mongoose.connect(
     'mongodb+srv://recofka:recofkadb@cluster0-fyhzz.mongodb.net/dropBox?retryWrites=true',
     {
@@ -11,9 +23,16 @@ mongoose.connect(
     }
 );
 
+
+//middleware socket io
+app.use((req, res, next) => {
+    req.io = io
+    return next();
+});
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('./files', express.static(path.resolve(__dirname, '..', 'tmp')))
 app.use(require('./routes'));
 
-app.listen(5000);
+server.listen(5000);
