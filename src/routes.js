b6a905fd576f1f8cede24cca4c0440cce0e004de
recofka <recofka@gmail.com>
2019-04-15 12:03:11 +0200

    create box controller and post box

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const routes = express.Router();
+const BoxController = require('./controllers/BoxController');
+
 
 routes.get("/", (req, res) =>{
     return res.send("basic server is running")
 });
+
+routes.post("/boxes", BoxController.store);
 
 module.exports = routes;
